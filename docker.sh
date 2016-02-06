0750240a322c63673f4b3ac9686d76680d4ca161
Marcus Bauer <mabako@gmail.com>
2016-02-06 20:57:53 +0100

    v1.2-beta.5

---
+++ docker.sh
@@ -0,0 +1,9 @@
+docker run \
+  -v ~/fdroid/config.py:/fdroid/config.py \
+  -v ~/fdroid/sg-icon.png:/fdroid/sg-icon.png \
+  -v ~/fdroid/metadata/:/fdroid/metadata/ \
+  -v ~/fdroid/steamgifts-android.jks:/fdroid/steamgifts-android.jks \
+  -v ~/fdroid/repo:/fdroid/repo \
+  --entrypoint sh \
+  fdroid \
+  -c "fdroid build -v net.mabako.steamgifts.beta && fdroid publish && fdroid update"
